a9aeaebaf9f9a15aa6bdac4817ff4aff4da4b06f
ssw0420 <tmddn5645@gmail.com>
2024-01-25 16:47:49 +0900

    Crossbow Line Renderer
    2024.01.25
    Crossbow Line Renderer Update
    광선 위치 설정, 크로스보우 Grip 비활성화 설정

---
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/CrossbowController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CrossbowController : MonoBehaviour
+{
+
+}
