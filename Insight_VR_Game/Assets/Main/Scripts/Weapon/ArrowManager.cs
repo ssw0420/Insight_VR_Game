5b22718322c973889752e0507235f09a9725e8f7
ssw0420 <tmddn5645@gmail.com>
2024-01-26 17:31:03 +0900

    Crossbow Shoot Setting
    2024.01.26
    크로스보우 프리팹 설정 변경, 화살 발사 설정
    화살 궤도 미구현상태

--- Insight_VR_Game/Assets/Main/Scripts/Weapon/ArrowManager.cs
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/ArrowManager.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class ArrowManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject crossbow;
+    Rigidbody rigid;
+    TrailRenderer trailRenderer;
+    public float angle;
+    public float power;
+    Vector3 v1;
+    float Timedir;
+    float gravity;
+
+    public void GetCrossbow(GameObject crossbow)
     {
-        
+        this.crossbow = crossbow;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        trailRenderer = transform.Find("MoveTrack").GetComponent<TrailRenderer>();
     }
+
+    private void Start()
+    {
+        transform.localPosition = new Vector3(0, 0.16572f, 0.20547f);
+        trailRenderer.enabled = false;
+        Timedir = Time.deltaTime;
+        gravity = -(1.0f * Timedir * Timedir / 2.0f);
+    }
+
+    public void Fire()
+    {
+        rigid.isKinematic = false;
+        trailRenderer.enabled = true;
+        rigid.useGravity = true;
+        gameObject.transform.SetParent(null);
+
+        StartCoroutine("Move");
+    }
+
+    IEnumerator Move()
+    {
+        while (true)
+        {
+            yield return null;
+            Timedir += Time.deltaTime;
+            v1.z = power = Mathf.Cos(angle * Mathf.PI / 180.0f) * Timedir;
+            v1.y = power = Mathf.Cos(angle * Mathf.PI / 180.0f) * Timedir * gravity;
+            transform.Translate(v1);
+
+            transform.Rotate(new Vector3(Mathf.Cos(angle * Mathf.PI / 180.0f), 0, 0));
+        }
+    }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     // if (other.CompareTag("Monster"))
+    //     // {
+    //     //     Debug.Log("적중");
+    //     //     Destroy(gameObject);
+
+    //     //     var MonsterScript = other.GetComponent<Monster>();
+    //     //     MonsterScript.health -= 1;
+    //     //     if (MonsterScript.health <= 0)
+    //     //         MonsterScript.Die();
+    //     //     else
+    //     //         MonsterScript.Hit();
+    //     // }
+
+    //     // if (other.CompareTag("terrain"))
+    //     // {
+    //     //     Debug.Log("지형 오브젝트에 적중");
+    //     //     Destroy(gameObject);
+    //     // }
+    // }
 }
--- Insight_VR_Game/Assets/Main/Scripts/Weapon/CrossbowController.cs
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/CrossbowController.cs
@@ -5,8 +5,58 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class CrossbowController : MonoBehaviour
 {
+    private Animator animator;
+    private bool isReload = false;
 
+    public float shootDelay = 1f;
+
+    public GameObject arrowPrefab;
+    // public Transform shootPoint;
+
+    [SerializeField] ArrowManager arrowManagerScript;
+
+    // public UnityEvent<Vector3> OnShootSuccess;
+    // public UnityEvent OnShootFail;
+
+    private void Awake()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, transform);
+        arrowManagerScript = arrow.GetComponent<ArrowManager>();
+        // arrowManagerScript.GetCrossbow(gameObject);
+    }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        LoadArrow();
+    }
+
+    private IEnumerator Fill()
+    {
+        var wfs = new WaitForSeconds(shootDelay);
+        yield return wfs;
+
+        isReload = false;
+        LoadArrow();
+    }
+
+    public void Shoot()
+    {
+        if(isReload)
+            return;
+
+        isReload = true;
+        // StopAllCoroutines();
+        StartCoroutine(Fill());
+        arrowManagerScript.Fire();
+    }
+
+    void LoadArrow()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, transform);
+        arrowManagerScript = arrow.GetComponent<ArrowManager>();
+    }
 }
