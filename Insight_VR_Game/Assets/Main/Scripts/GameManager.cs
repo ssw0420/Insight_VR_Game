cb7ea567300357a5bd5479beacb6b06c5db778cd
MinHyeong <91134812+MinHyeo@users.noreply.github.com>
2024-02-02 15:35:50 +0900

    Revert "."
    This reverts commit e540815a349983cfc3f184e874a6617b3066e8b5.

---
+++ Insight_VR_Game/Assets/Main/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    protected static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [SerializeField] int stage = 1;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public int GetStage()
+    {
+        return stage;
+    }
+}
