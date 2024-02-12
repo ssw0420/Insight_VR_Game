c021d1be9e079ae70f211ff8469e802f9d469d59
MinHyeo <p610222@naver.com>
2024-02-12 15:13:31 +0900

    HealthMonster Update
    체력 몬스터 틀만 구현

---
+++ Insight_VR_Game/Assets/Main/Scripts/HealthMonster.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMonster : Monster
+{
+    private void Start()
+    {
+        health = 1;
+    }
+}
