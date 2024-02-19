5f4711a6ecb700c052e399efe3cea63ca2aa564f
MinHyeong <91134812+MinHyeo@users.noreply.github.com>
2024-02-19 17:55:01 +0900

    UI VR에 맞게 위치 수정
    라운드바와 체력바 위치 수정

--- Insight_VR_Game/Assets/Main/Scripts/Card/ChoiceCard.cs
+++ Insight_VR_Game/Assets/Main/Scripts/Card/ChoiceCard.cs
@@ -11,7 +11,8 @@ public class ChoiceCard : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(Card_Prefab);
+        Destroy(gameObject); // 카드에 정해진 스킬이나 강화를 해주고 Destroy 해주기
+        Destroy(Card_Prefab); // 카드를 선택하면 전체 카드를 삭제 해 줘야 되는데 prefab 자체를 삭제할 수 있기 때문에 객체를 따로 만들어 줘서
+                              // 참조한 것으로 destroy 해주기
     }
 }
