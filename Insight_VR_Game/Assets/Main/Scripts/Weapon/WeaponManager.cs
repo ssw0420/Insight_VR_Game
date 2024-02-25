5b4c8f9d8c24eb4b9c6ccfcef862148d21919d39
MinHyeo <p610222@naver.com>
2024-02-25 21:34:35 +0900

    save
    save

---
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/WeaponManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    [Header("Weapon Object")]
+    [SerializeField] GameObject iceBall;
+    [SerializeField] GameObject blackHole;
+}
