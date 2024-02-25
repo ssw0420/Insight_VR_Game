9de0809b767c3550ff1569741ee850323d714908
yumin han <gksdbals20@naver.com>
2024-02-25 17:46:45 +0900

    Revert "Revert "Card(스킬 X)""
    This reverts commit ca62ebc213e4b8d134519af4e5232b7566b54776.

---
+++ Insight_VR_Game/Assets/Main/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public bool BlackHoleState = false;
+    public bool IceState = false;
+    public bool DmgState = false;
+    public bool HealthState = false;
+
+}
