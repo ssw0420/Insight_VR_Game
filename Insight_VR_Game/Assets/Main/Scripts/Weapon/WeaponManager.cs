85fbddc3582a2f23f9cced97b9cfbbad4d0a6867
MinHyeong <91134812+MinHyeo@users.noreply.github.com>
2024-02-27 13:46:09 +0900

    .
    .

--- Insight_VR_Game/Assets/Main/Scripts/Weapon/WeaponManager.cs
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/WeaponManager.cs
@@ -74,6 +74,9 @@ public class WeaponManager : MonoBehaviour
             if (s_State[(int)SkillType.Ice] == SkillState.Fire || s_State[(int)SkillType.BlackHole] == SkillState.Fire)
                 return;
 
+            if (s_State[(int)SkillType.Ice] == SkillState.Reloading || s_State[(int)SkillType.BlackHole] == SkillState.Reloading)
+                return;
+
             switch (s_Type)
             {
                 case SkillType.Ice:
@@ -191,6 +194,7 @@ public class WeaponManager : MonoBehaviour
         {
             iceLance_time = 0.0f;
             GameObject iceLance = Instantiate(skillPrefab[(int)SkillType.Ice], transform);
+            iceLance.transform.position += Vector3.forward * 0.1f;
         }
 
         if (using_time[(int)SkillType.Ice] <= 0.0f)
