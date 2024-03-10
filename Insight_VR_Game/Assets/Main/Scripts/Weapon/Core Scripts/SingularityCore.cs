b45559631ca8ebff74d227c747dcdecdb70b255b
MinHyeong <91134812+MinHyeo@users.noreply.github.com>
2024-03-10 19:26:00 +0900

    Update BlackHole, GameManager
    블랙홀에서 1초마다 데미지를 입히는 기능 구현,
    블랙홀 크기 조절,
    Game 끝나면 여러 엔딩이 나오는 버그 수정
    
    Co-Authored-By: ssw0420 <129267014+ssw0420@users.noreply.github.com>
    Co-Authored-By: yumin han <85437806+YuMinHan01@users.noreply.github.com>

--- Insight_VR_Game/Assets/Main/Scripts/Weapon/Core Scripts/SingularityCore.cs
+++ Insight_VR_Game/Assets/Main/Scripts/Weapon/Core Scripts/SingularityCore.cs
@@ -1,17 +1,53 @@
 ﻿using System.Collections;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.XR.Interaction;
 [RequireComponent(typeof(SphereCollider))]
 public class SingularityCore : MonoBehaviour
 {
-/*    void OnTriggerStay (Collider other) {
-        if(other.GetComponent<SingularityPullable>()){
-            //other.GetComponent<Monster>().OnHit();
-        }
-    }*/
+    private SphereCollider sphereCollider;
+    /*    void OnTriggerStay (Collider other) {
+            if(other.GetComponent<SingularityPullable>()){
+                //other.GetComponent<Monster>().OnHit();
+            }
+        }*/
 
-    void Awake(){
-        if(GetComponent<SphereCollider>()){
-            GetComponent<SphereCollider>().isTrigger = true;
+    //GameObject RoundObject;
+    float time = 0;
+    bool isBomb = false;
+    //void Awake(){
+    //    RoundObject = GetComponentInChildren<GameObject>();
+    //}
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+
+        if(time >= 1f && time <= 1.5f)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            isBomb = true;
+            Debug.Log("1.0");
+        }
+        else if(time > 1.5f)
+        {
+            Debug.Log("1.5");
+            time = 0;
+            isBomb = false;
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Monster") || other.gameObject.layer == LayerMask.NameToLayer("Health Monster"))
+        {
+            Debug.Log("onBlackHoleHitMonster");
+            if (!isBomb)
+                return;
+
+            other.GetComponent<Monster>().OnHit(1, "BlackHole");
+            Debug.Log("ontrigger");
         }
     }
 }
